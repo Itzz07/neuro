13ab533ac3de46fd88461f5eec634c4e7b15b2a4
itzz07 <zimbajoel1@gmail.com>
2025-02-02 14:03:24 +0200

    Initial commit

--- app.py
+++ app.py
@@ -107,4 +107,5 @@ def get_training_status():
     return jsonify(training_status)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+    app.run(threaded=True, debug=True)
+    # app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
